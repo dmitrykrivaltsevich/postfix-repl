c764ad32c3b2691d1e52cae4c8aa5df6f0bee801
Dmitry Krivaltsevich <dmitry.krivaltsevich@gmail.com>
2017-07-20 13:50:13 +0200

    #4 added interpreter to the REPL

--- src/main/scala/com/dmitrykrivaltsevich/postfixrepl/interpreter/PostFixRepl.scala
+++ src/main/scala/com/dmitrykrivaltsevich/postfixrepl/interpreter/PostFixRepl.scala
@@ -3,15 +3,29 @@ package com.dmitrykrivaltsevich.postfixrepl.interpreter
 class PostFixRepl {
 
   private var isRunning = true
+  private val interpreter = new PostFixInterpreter(0, Nil)
 
-  def loop(in: => String, out: String => Unit): Unit =
+  def loop(in: => String, out: String => Unit, err: String => Unit): Unit =
     while (isRunning)
       in match {
-        case "exit" | ":q" => {
+        case "exit" | ":q" =>
           isRunning = false
           out("Exiting...")
-        }
-        case source => out(source)
+
+        case source =>
+          val commands = parse(source)
+          interpreter.eval(commands, List.empty) match {
+            case ProgramSuccess(value) => out(value.toString)
+            case ProgramFailure(message) => err(message)
+          }
       }
 
+  // TODO: ad-hoc implementation, replace by parser
+  private def parse(str: String): List[PostFixCommand] =
+    str.drop(1) // left  '('
+      .dropRight(1) // right ')'
+      .split(" ") // get tokens
+      .drop(2) // remove 'postfix' and number of arguments
+      .map(token => NumberCommand(token.toInt))
+      .toList
 }
--- src/main/scala/com/dmitrykrivaltsevich/postfixrepl/ui/CommandLine.scala
+++ src/main/scala/com/dmitrykrivaltsevich/postfixrepl/ui/CommandLine.scala
@@ -2,7 +2,7 @@ package com.dmitrykrivaltsevich.postfixrepl.ui
 
 import com.dmitrykrivaltsevich.postfixrepl.interpreter.PostFixRepl
 
-import scala.Console.{GREEN, RESET}
+import scala.Console.{GREEN, RESET, RED}
 import scala.annotation.tailrec
 import scala.io.StdIn
 
@@ -16,11 +16,11 @@ object CommandLine extends App {
   )
 
   val repl = new PostFixRepl
-  repl.loop(in, out)
+  repl.loop(in, out, err)
 
   @tailrec
   private def in: String = {
-    Console.print(s"${RESET}${GREEN}>${RESET} ")
+    Console.print(s"$RESET$GREEN>$RESET ")
     StdIn.readLine() match {
       case "" => in
       case str => str
@@ -29,5 +29,8 @@ object CommandLine extends App {
 
   private def out(msg: String): Unit =
     Console.print(s"$msg\n\n")
+
+  private def err(msg: String): Unit =
+    out(s"$RESET${RED}error: $msg$RESET")
 
 }
